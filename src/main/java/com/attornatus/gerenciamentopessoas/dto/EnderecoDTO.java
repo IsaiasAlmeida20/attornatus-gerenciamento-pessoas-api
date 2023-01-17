df7a99987fa4e41969460e4fe4a7ffd95b090d40
Isaias Almeida <isaiasalmeida319@gmail.com>
2023-01-17 11:30:03 -0300

    :sparkles: feat: Implementing dto and service of endereco

---
+++ src/main/java/com/attornatus/gerenciamentopessoas/dto/EnderecoDTO.java
@@ -0,0 +1,75 @@
+package com.attornatus.gerenciamentopessoas.dto;
+
+import java.io.Serializable;
+
+import com.attornatus.gerenciamentopessoas.entities.Endereco;
+
+public class EnderecoDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String logradouro;
+	private Long cep;
+	private Integer numero;
+	private String cidade;
+	
+	public EnderecoDTO() {
+	}
+
+	public EnderecoDTO(Long id, String logradouro, Long cep, Integer numero, String cidade) {
+		this.id = id;
+		this.logradouro = logradouro;
+		this.cep = cep;
+		this.numero = numero;
+		this.cidade = cidade;
+	}
+	
+	public EnderecoDTO(Endereco entity) {
+		this.id = entity.getId();
+		this.logradouro = entity.getLogradouro();
+		this.cep = entity.getCep();
+		this.numero = entity.getNumero();
+		this.cidade = entity.getCidade();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public Long getCep() {
+		return cep;
+	}
+
+	public void setCep(Long cep) {
+		this.cep = cep;
+	}
+
+	public Integer getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	
+}
---
+++ src/main/java/com/attornatus/gerenciamentopessoas/services/EnderecoService.java
@@ -0,0 +1,24 @@
+package com.attornatus.gerenciamentopessoas.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.attornatus.gerenciamentopessoas.dto.EnderecoDTO;
+import com.attornatus.gerenciamentopessoas.entities.Endereco;
+import com.attornatus.gerenciamentopessoas.repositories.EnderecoRepository;
+
+@Service
+public class EnderecoService {
+
+	@Autowired
+	private EnderecoRepository repository;
+	
+	@Transactional(readOnly = true)
+	public Page<EnderecoDTO> findAllPaged(Pageable pageable) {
+		Page<Endereco> list = repository.findAll(pageable);
+		return list.map(x -> new EnderecoDTO(x));
+	}
+}
