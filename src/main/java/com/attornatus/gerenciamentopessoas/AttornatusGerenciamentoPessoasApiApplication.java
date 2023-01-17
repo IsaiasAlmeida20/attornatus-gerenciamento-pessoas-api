2051eeece144f65318614de38af3b89758bb8e17
Isaias Almeida <isaiasalmeida319@gmail.com>
2023-01-17 10:51:10 -0300

    :tada: First commit

---
+++ src/main/java/com/attornatus/gerenciamentopessoas/AttornatusGerenciamentoPessoasApiApplication.java
@@ -0,0 +1,13 @@
+package com.attornatus.gerenciamentopessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttornatusGerenciamentoPessoasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttornatusGerenciamentoPessoasApiApplication.class, args);
+	}
+
+}
