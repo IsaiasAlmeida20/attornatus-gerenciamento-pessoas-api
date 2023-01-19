1e62e054ddc38240df03ccd3ac918cbb13312d7f
Isaias Almeida <isaiasalmeida319@gmail.com>
2023-01-19 10:15:39 -0300

    :recycle: refactor: update import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,8 +1,8 @@
-insert into tb_pessoa (id, nome, data_nascimento) values (1, 'Isaias', '1998-05-12');
-insert into tb_pessoa (id, nome, data_nascimento) values (2, 'Edirene', '1999-04-01');
+insert into tb_person (name, birth_date) values ('Isaias', '1998-05-12');
+insert into tb_person (name, birth_date) values ('Edirene', '1999-04-01');
 
-insert into tb_endereco (logradouro, cep, numero, cidade, pessoa_id) values ('Rua A', 100000, 1, 'Mogi das Cruzes', 1);
-insert into tb_endereco (logradouro, cep, numero, cidade, pessoa_id) values ('Rua B', 110000, 2, 'Itu', 2);
+insert into tb_addres (public_place, zip_code, number, city) values ('Rua A', 100000, 1, 'Mogi das Cruzes');
+insert into tb_addres (public_place, zip_code, number, city) values ('Rua B', 110000, 2, 'Itu');
 
-
-
+INSERT INTO tb_person_address (person_id, address_id) VALUES (1, 2);
+INSERT INTO tb_person_address (person_id, address_id) VALUES (2, 2);
